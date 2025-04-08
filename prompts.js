bc2116a7ae5cd42f3b197502b76ad555b5fa4855
Rain Jocas <rainjocas@gmail.com>
2025-04-08 12:28:56 -0400

    better storage (I think)

--- prompts.js
+++ prompts.js
@@ -1,38 +1,59 @@
-/* save prompt 1 */
-prompt_1 = document.getElementById("select-prompt-1").value;
-sessionStorage.setItem("prompt_1", prompt_1);
 
-/* save response 1 */
-response_1 = document.getElementById("response-1").value;
-sessionStorage.setItem("response_1", response_1);
+question_1 = []
+question_2 = []
+question_3 = []
 
-/* remove prompt 1 from questions */
-document.getElementById("select-prompt-2").options.remove(prompt_1);
-document.getElementById("select-prompt-3").options.remove(prompt_1);
+/* store prompt & response 1 */
+function store_responses_1() {
+    selectedPrompt = document.getElementById("select-prompt-1");
+    selectedResponse = document.getElementById("response-1");
+    const prompt = selectedPrompt.value;
+    localStorage.setItem("prompt_1", prompt);
+    const response = selectedResponse.value;
+    localStorage.setItem("response-1", response);
+    question_1 = [localStorage.getItem("prompt_1"), localStorage.getItem("response-1")]
+}
 
-/* save prompt 2 */
-prompt_2 = document.getElementById("select-prompt-2").value;
-sessionStorage.setItem("prompt_2", prompt_2);
+/* store prompt & response 2 */
+function store_responses_2() {
+    selectedPrompt = document.getElementById("select-prompt-2");
+    selectedResponse = document.getElementById("response-2");
+    const prompt = selectedPrompt.value;
+    localStorage.setItem("prompt_2", prompt);
+    const response = selectedResponse.value;
+    localStorage.setItem("response-2", response);
+    question_2 = [localStorage.getItem("prompt_2"), localStorage.getItem("response-2")]
+}
 
-/* save response 2 */
-response_2 = document.getElementById("response-2").value;
-sessionStorage.setItem("response_2", response_2);
+/* store prompt & response 3 */
+function store_responses_3() {
+    selectedPrompt = document.getElementById("select-prompt-3");
+    selectedResponse = document.getElementById("response-3");
+    const prompt = selectedPrompt.value;
+    localStorage.setItem("prompt_3", prompt);
+    const response = selectedResponse.value;
+    localStorage.setItem("response-3", response);
+    question_3 = [localStorage.getItem("prompt_3"), localStorage.getItem("response-3")]
+}
 
-/* remove prompt 2 from questions */
-document.getElementById("select-prompt-3").options.remove(prompt_2);
 
-/* save prompt 3 */
-prompt_3 = document.getElementById("select-prompt-3").value;
-sessionStorage.setItem("prompt_3", prompt_3);
+/* remove first prompt from pages */
+function removeFirst() {
+    const prompt_1 = localStorage.getItem(prompt_1)
+    document.getElementById("select-prompt-2").options.remove(prompt_1);
+    document.getElementById("select-prompt-3").options.remove(prompt_1);
+}
 
-/* save response 3 */
-response_3 = document.getElementById("response-3").value;
-sessionStorage.setItem("response_3", response_3);
+/* remove second prompt from last page */
+function removeSecond() {
+    const prompt_2 = localStorage.getItem(prompt_2)
+    document.getElementById("select-prompt-3").options.remove(prompt_2);
+}
 
 usersList = {
     user_1 : {
-        prompt1: [prompt_1, response_1],
-        prompt2: [prompt_2, response_2],
-        prompt3: [prompt_3, response_3]
+        prompt1: question_1,
+        prompt2: question_2,
+        prompt3: question_3
     }
 }
