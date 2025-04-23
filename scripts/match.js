d13ab4fd4fccd9af76cd375c73c2c95e83c28aa7
Rain Jocas <rainjocas@gmail.com>
2025-04-22 21:12:51 -0400

    basic layering set up

--- scripts/match.js
+++ scripts/match.js
@@ -1,5 +1,9 @@
+import { userDistances } from './algorithm.js';
+
 distances = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50]
 angles = [0, 36, 72, 108, 144, 180, 216, 252, 288, 324, 360]
+
+matches = [[0, 0.1], [0, 0.2], [0, 0.3], [0, 0.4], [0, 0.5], [0, 0.6], [0, 0.7], [0, 0.8], [0, 0.9], [0, 1]]
 
 var numMatches = 10;
 
@@ -9,17 +13,25 @@ centerY = 250
 Xs = [50, 100, 150, 400, 200, 300, 380, 400, 300, 500]
 Ys = [50, 10, 400, 50, 250, 75, 350, 200, 450, 10]
 
+const base = new Image();
+base.src = "images/prof_customization/base.svg";
+
 function drawGraph() {
 
     const c = document.getElementById("matchesGraph");
     const ctx = c.getContext("2d");
 
     for (i = 0; i < 10; i++) {
+        id = matches[i][0]
+        var pfColor = '#007AFF' //NOTE: get color from id
+        distance = matches[i][1]
+
+
         //calculate circle centerpoints
         radians = angles[i] * Math.PI/180
-        endX = distances[i] * Math.cos(radians) * 5
+        endX = distance * Math.cos(radians) * 250
         endX = endX + centerX
-        endY = distances[i] * Math.sin(radians) * 5
+        endY = distance * Math.sin(radians) * 250
         endY = endY + centerY
 
         //draw a line connecting to the circle
@@ -33,13 +45,21 @@ function drawGraph() {
 
         //draw a circle
         ctx.beginPath(); // Start a new path
-        ctx.arc(endX, endY, 10, 0, 2 * Math.PI); // Create the circle arc
-        ctx.strokeStyle = '#007AFF'; // Set the stroke color
-        ctx.lineWidth = 3; // Set the line width
-        ctx.fillStyle = '#007AFF'
-        ctx.fill()
-        ctx.stroke(); // Draw the circle
+        ctx.arc(endX, endY, 20, 0, 2 * Math.PI); // Create the circle arc
+        ctx.strokeStyle = 'black' ; // Set the stroke color
+        ctx.lineWidth = 2; // Set the line width
+        ctx.fillStyle = pfColor;
+        ctx.fill();
+        ctx.stroke()
         ctx.closePath();
+
+        //place profile
+        ctx.drawImage(base, endX - 18, endY - 18, 36, 36);
+
+        //accesories
+        const accesory = new Image();
+        accesory.src = "images/prof_customization/beanie.svg"; //NOTE: grab accesories from id
+        ctx.drawImage(accesory, endX - 18, endY - 18, 36, 36);
     }
 
     ctx.beginPath(); // Start a new path
