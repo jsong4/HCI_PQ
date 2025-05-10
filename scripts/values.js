fb1e9348e1871ef3b59e49cad0ff10f7f335b4ff
Rain Jocas <rainjocas@gmail.com>
2025-05-10 14:39:22 -0400

    dealbreakers options fixed

--- scripts/values.js
+++ scripts/values.js
@@ -36,3 +36,54 @@ function updateUser() {
     // Store updated data
     localStorage.setItem("usersList", JSON.stringify(usersList));
 }
+
+const UsersList = {
+    "Snergle": {
+       "prompt1": ["In a parallel universe, I'm definitely...", "A polar bear who noms on fish all day"],
+       "prompt2": ["One rule of society I'd totally rewrite...", "SUPERS MUST HAVE A SET MENU! I NEED TO KNOW THE MOZZIE STICK SCHEDULE."],
+       "prompt3": ["A letter to future me would definitely include...", "I hope you made 1 million dollars, my lakehouse dreams are counting on it."],
+       "checkbox": ["Chronically Online", "Punctual", "Easygoing", "Outgoing", "Memecentral"],
+       "profile": {
+            "color": "#738cff",
+           "accessory": "images/prof_customization/glasses.svg"
+        },
+       "selectedPrompts": [
+           "In a parallel universe, I'm definitely...",
+           "One rule of society I'd totally rewrite...",
+           "A letter to future me would definitely include..."
+       ],
+       "password": "123"
+   },
+    "Bun Bo Hue": {
+       "prompt1": ["The soundtrack of my life would definitely include...", "Call Me Maybe by Carly Rae Jepsen (Lawnmower Edition)"],
+       "prompt2": ["If my life were a meme right now, it would be...", "The 'huh' cat."],
+       "prompt3": ["If my personality were a cocktail, it would be called...", "Le Fat Bastard, not a cocktail but the hippo doing a handstand just resonates with me."],
+       "checkbox": ["Easily Peer-Pressured", "Listener", "Honest", "Adventurous"],
+       "profile": {
+           "color": "#bd4fb0",
+           "accessory": "images/prof_customization/bow.svg"
+       },
+       "selectedPrompts": [
+           "The soundtrack of my life would definitely include...",
+           "If my life were a meme right now, it would be...",
+           "If my personality were a cocktail, it would be called..."
+       ],
+       "password": "123"
+   },
+    "Ooonga Boonga": {
+       "prompt1": ["If I could have one superpower, it would be...", "Absolutely decimating Earth's population of fish and having a infinite stomach to eat sushi 24/7 (sorry fish welfare, but me like fish)."],
+       "prompt2": ["In a parallel universe, I'm definitely...", "A hyrax. I will dig my way into people's houses, give them the bombastic side eye, and start screaming 'AWAWA!'"],
+       "prompt3": ["Three dinner guests, unlimited wine, and chaos ensues...", "Snoop Dogg, Martha Stewart, and Bob Ross. And instead of wine, it will a 'not-unexpected, but wholesome' blunt rotation."],
+       "checkbox": ["Sporty", "Optimistic", "Empathetic", "Chronically Online"],
+       "profile": {
+           "color": "#668a75",
+           "accessory": "images/prof_customization/mustache.svg"
+       },
+       "selectedPrompts": [
+           "If I could have one superpower, it would be...",
+           "In a parallel universe, I'm definitely...",
+           "Three dinner guests, unlimited wine, and chaos ensues..."
+       ],
+       "password": "123"
+   }
+}
